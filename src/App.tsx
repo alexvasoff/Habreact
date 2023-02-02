34c3bad69a2c9ff3b42751c7b8dc1311bc042a3e
Aleksey Vasilev <alexvasoff@yandex.ru>
2023-02-02 10:52:56 +0300

    feat: Add test pages to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
 import Counter from "./components/Counter";
 import './index.scss';
+import {AboutPageLazy} from "./pages/AboutPage/AboutPageLazy";
+import {MainPageLazy} from "./pages/MainPage/MainPageLazy";
+
 const App = () => {
     return (
         <div className={'app'}>
+            <Link to={'/about'}>О приложении</Link>
+            <Link to={'/'}>Главная</Link>
+            <Suspense fallback={<div>Загрузка...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageLazy/>}/>
+                    <Route path={'/'} element={<MainPageLazy/>}/>
+                </Routes>
+            </Suspense>
             <h1>HEllo</h1>
-         <Counter />
+            <Counter/>
         </div>
     );
 };
