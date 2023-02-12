4ce753be0fc19bd14b46f46739d9a43254bdb677
Aleksey Vasilev <alexvasoff@yandex.ru>
2023-02-13 00:13:27 +0300

    feat: Add Navbar widget

---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import cls from './Navbar.module.scss'
+import {AppLink} from "@/shared/ui/AppLink/AppLink";
+import {classNames} from '@/shared/lib/classNames/classNames';
+
+
+interface NavbarProps {
+    className?: string
+}
+export const Navbar = (props: NavbarProps) => {
+    return (
+        <div className={classNames(cls.navbar, {}, [props.className])}>
+            <div className={cls.appName}></div>
+            <div className={cls.links}>
+                <AppLink to={'/about'}>О приложении</AppLink>
+                <AppLink to={'/'}>Главная</AppLink>
+            </div>
+        </div>
+    );
+};
