dacc6f17f150e903307c7705ef7107f21357a293
Aleksey Vasilev <alexvasoff@yandex.ru>
2023-02-13 00:13:11 +0300

    feat: Add AppLink shared component

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,21 @@
+import {classNames} from "@/shared/lib/classNames/classNames";
+import {Link, LinkProps} from "react-router-dom";
+import cls from './AppLink.module.scss'
+
+export enum AppLinkTheme {
+    PRIMARY = 'primary',
+    SECONDARY = 'secondary',
+}
+
+interface AppLinkProps extends LinkProps {
+    className?: string
+    theme?: AppLinkTheme
+}
+export const AppLink = (props: AppLinkProps) => {
+    const {children, className, to, theme = AppLinkTheme.PRIMARY, ...otherProps} = props;
+    return (
+        <Link to={to} className={classNames(cls.appLink, {}, [className, cls[theme]])}>
+            {children}
+        </Link>
+    );
+};
