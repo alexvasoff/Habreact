8661d9f22f6226059c5c98ebc963c8cef92f0d38
Aleksey Vasilev <alexvasoff@yandex.ru>
2023-02-12 23:58:24 +0300

    feat: Set default values to classNames params

--- src/shared/lib/classNames/classNames.tsx
+++ src/shared/lib/classNames/classNames.tsx
@@ -1,5 +1,5 @@
 type Mods = Record<string, boolean | string>;
-export function classNames(cls: string, mods: Mods, additional: string[]) {
+export function classNames(cls: string, mods: Mods = {}, additional: string[] = []) {
     const activeMods = Object.entries(mods).filter(([className, isActive]) => !!isActive).map(([className]) => className);
     return [
         cls,
