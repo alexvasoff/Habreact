a9f8d681733c926fcdc9b0a7557b57d1764aee02
Aleksey Vasilev <alexvasoff@yandex.ru>
2023-02-14 00:33:55 +0300

    feat: Export Theme type

--- src/app/providers/ThemePovider/index.ts
+++ src/app/providers/ThemePovider/index.ts
@@ -1,2 +1,4 @@
-export { default } from "./ui/ThemeProvider";
-export { useTheme } from "./lib/useTheme";
+export {default} from "./ui/ThemeProvider";
+export {useTheme} from "./lib/useTheme";
+export {Theme} from "./lib/ThemeContext";
+
