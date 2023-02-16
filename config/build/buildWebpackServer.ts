013961d3cef43b20a0c77a39eef45333c3802c46
Aleksey Vasilev <alexvasoff@yandex.ru>
2023-02-17 01:39:32 +0300

    fix: Remove static option from devServer

--- config/build/buildWebpackServer.ts
+++ config/build/buildWebpackServer.ts
@@ -3,7 +3,6 @@ import type { Configuration as DevServConfiguration } from 'webpack-dev-server';
 export function buildWebpackServer(options: BuildOptions): DevServConfiguration {
     return {
         port: options.port,
-        static: options.paths.output,
         historyApiFallback: true
     }
 }
