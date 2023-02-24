d3db1658334c0ee8a6ef286046c9fc74d28af414
Aleksey Vasilev <alexvasoff@yandex.ru>
2023-02-24 15:50:20 +0300

    feat: delete notFound empty styles

--- src/pages/NotFound/ui/NotFound.tsx
+++ src/pages/NotFound/ui/NotFound.tsx
@@ -1,6 +1,5 @@
 import { useTranslation } from 'react-i18next';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import cls from './NotFound.module.scss';
 
 interface NotFoundProps {
     className?: string
@@ -10,7 +9,7 @@ export const NotFound = (props: NotFoundProps) => {
   // eslint-disable-next-line no-undef
   const { t } = useTranslation();
   return (
-      <div className={classNames(cls.notFound, {}, [className])}>
+      <div className={classNames('', {}, [className])}>
           <h1>{t('Страница не найдена')}</h1>
       </div>
   );
