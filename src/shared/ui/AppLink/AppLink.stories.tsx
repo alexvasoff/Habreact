7465cec9ddce4e3b8cc0bab79b0706f699b593d8
Aleksey Vasilev <alexvasoff@yandex.ru>
2023-02-24 12:31:47 +0300

    feat: add appLink story

---
+++ src/shared/ui/AppLink/AppLink.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { themeDecorators } from '@/shared/config/storybook/themeDecorators/themeDecorators';
+import { Theme } from '@/app/providers/ThemePovider';
+import { AppLink, AppLinkTheme } from './AppLink';
+
+export default {
+  title: 'shared/AppLink',
+  component: AppLink,
+  args: {
+    to: '/',
+  },
+} as ComponentMeta<typeof AppLink>;
+
+const Template: ComponentStory<typeof AppLink> = (args) => <AppLink {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  children: 'Link text',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  children: 'Link text',
+  theme: AppLinkTheme.SECONDARY,
+};
+
+export const PrimaryDark = Template.bind({});
+PrimaryDark.args = {
+  children: 'Link text',
+};
+
+PrimaryDark.decorators = [themeDecorators(Theme.DARK)];
+export const SecondaryDark = Template.bind({});
+SecondaryDark.args = {
+  children: 'Link text',
+  theme: AppLinkTheme.SECONDARY,
+};
+SecondaryDark.decorators = [themeDecorators(Theme.DARK)];
